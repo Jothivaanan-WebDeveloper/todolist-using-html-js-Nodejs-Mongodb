86cc5aa7a126740758ae37692ffce4825979517c
Joe <jothivaananwebdeveloper@gmail.com>
2023-01-15 13:10:24 +0530

    New PORT

--- app.js
+++ app.js
@@ -144,6 +144,7 @@ app.get("/:customName", function(req,res){
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
